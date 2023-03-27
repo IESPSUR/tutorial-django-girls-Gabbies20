a0c2566cd0793067b31c47e85985f9c0ae9168ff
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-03-27 12:40:28 +0200

    Ejemplo de QuerySet

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
+
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+
+def mifuncion(request):
+    mensaje= "Gabriela"
+    mensaje2 = "Sokayna"
+    context = {}
+    context['key']= Post.objects.all()
+
+    return render(request, 'blog/index.html', context)
+
+
