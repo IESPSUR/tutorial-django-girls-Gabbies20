b34563f5f64c2f6785df489063d8f4226b33f062
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2023-04-10 02:33:30 +0200

    Agregadas vistas y plantilla para el detalle del post del blog así como también CSS para el sitio.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list,name='post_list'),
-    path('index/', views.mifuncion, name='index'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    #path('index/', views.mifuncion, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 
 from blog.models import Post
@@ -6,16 +6,21 @@ from blog.models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+   return render(request, 'blog/post_list.html', {'posts': posts})
 
 
-def mifuncion(request):
-    mensaje= "Gabriela"
-    mensaje2 = "Sokayna"
-    context = {}
-    context['key']= Post.objects.all()
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
 
-    return render(request, 'blog/index.html', context)
+
+#def mifuncion(request):
+#   mensaje= "Gabriela"
+#   mensaje2 = "Sokayna"
+#   context = {}
+#   context['key']= Post.objects.all()
+
+#   return render(request, 'blog/index.html', context)
 
 
